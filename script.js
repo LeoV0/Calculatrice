d8592b935ccb83014fc44907c954449b2265ab53
leoV0 <leo.cigale@gmail.com>
2025-05-13 15:09:12 +0200

    Input text disabled + ajout de math

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const buttonValue = document.querySelectorAll("#button");
+const buttonValue = document.querySelectorAll(".button");
 const displayCalc = document.querySelector("#displayNumber");
 const resultat = document.querySelector("#submitButton");
 const errorText = document.querySelector("#errorText");
@@ -7,13 +7,15 @@ const deletCalc = document.querySelector("#deletCalc");
 buttonValue.forEach((button) => {
   button.addEventListener("click", () => {
     displayCalc.value += button.value;
+    errorText.textContent = "";
   });
 });
 
 resultat.addEventListener("click", () => {
+  let expression = displayCalc.value;
+  errorText.textContent = "";
   try {
-    let expression = displayCalc.value;
-    let result = eval(expression);
+    let result = math.evaluate(expression);
     displayCalc.value = result;
   } catch {
     errorText.textContent = `Le calcul n'est pas valide`;
@@ -23,4 +25,5 @@ resultat.addEventListener("click", () => {
 
 deletCalc.addEventListener("click", () => {
   displayCalc.value = "";
+  errorText.textContent = "";
 });
